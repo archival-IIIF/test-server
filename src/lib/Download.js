685e12de2c99320b3e1675880abc4235193bbf94
Fabian <bart@macMini.local>
2018-09-12 08:10:59 +0200

    filename param for download added

--- src/lib/Download.js
+++ src/lib/Download.js
@@ -6,9 +6,13 @@ const readFileAsync = promisify(fs.readFile);
 const HttpError = require('../lib/HttpError');
 
 
-async function download(ctx, filePath) {
+async function download(ctx, filePath, fileName) {
     try {
+        if (!fileName) {
+            fileName = path.basename(filePath);
+        }
         ctx.set('Content-Type', mime.contentType(filePath));
+        ctx.set('Content-Disposition', 'attachment; filename="' + fileName + '"');
         ctx.body = await readFileAsync(filePath);
     }
     catch (err) {
