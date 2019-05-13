06dbd9b5bc3e78aa0764b8da6e773293783d61a4
Fabian <>
2019-05-13 09:41:00 +0200

    Update contentLanguage.js

--- src/language/contentLanguage.js
+++ src/language/contentLanguage.js
@@ -12,13 +12,74 @@ router.get('/collection/contentLanguage1', ctx => {
 });
 
 router.get('/collection/contentLanguage2', ctx => {
-    ctx.set('Content-Language', 'de-DE, en-US');
+    //ctx.set('Content-Language', 'de-DE, en-US');
     ctx.body = {
-        '@id': ctx.request.origin + '/collection/contentLanguage2',
+        '@id': ctx.request.origin + '/manifest/contentLanguage4',
         '@type': 'sc:Collection',
-        label: 'Content-language = de-De',
+        label: [
+            {"@value": "my label en", "@language": "en"},
+            {"@value": "my label de", "@language": "de"}
+        ],
         '@context': 'http://iiif.io/api/collection/2/context.json'
     };
 });
+
+router.get('/manifest/contentLanguage3', ctx => {
+    ctx.body = {
+        '@id': ctx.request.origin + '/manifest/contentLanguage3',
+        '@type': 'sc:Manifest',
+        label: {"@value": "my label", "@language": "en"},
+        description: {"@value": "Here is a longer description of the object", "@language": "en"},
+        '@context': 'http://iiif.io/api/collection/2/context.json',
+        mediaSequences: [{
+            '@id': ctx.request.origin + '/sequence/f113',
+            '@type': 'ixif:MediaSequence',
+            'elements': [{
+                '@id': ctx.request.origin + '/file/f113',
+                '@type': 'dctypes:Document',
+                'format': 'video/mp4',
+                'rendering': {
+                    '@id': ctx.request.origin + '/file/f113/original',
+                    'label': 'Original copy',
+                    'format': 'video/mp4'
+                }
+            }]
+        }]
+    };
+});
+
+
+router.get('/manifest/contentLanguage4', ctx => {
+    ctx.body = {
+        '@id': ctx.request.origin + '/manifest/contentLanguage4',
+        '@type': 'sc:Manifest',
+        label: [
+            {"@value": "my label en", "@language": "en"},
+            {"@value": "my label de", "@language": "de"}
+        ],
+        description: {"@value": "Here is a longer description of the object", "@language": "en"},
+        '@context': 'http://iiif.io/api/collection/2/context.json',
+        mediaSequences: [{
+            '@id': ctx.request.origin + '/sequence/f113',
+            '@type': 'ixif:MediaSequence',
+            'elements': [{
+                '@id': ctx.request.origin + '/file/f113',
+                '@type': 'dctypes:Document',
+                'format': 'video/mp4',
+                'rendering': {
+                    '@id': ctx.request.origin + '/file/f113/original',
+                    'label': 'Original copy',
+                    'format': 'video/mp4'
+                }
+            }]
+        }]
+    };
+});
+
+
+
+
+
+//{"description": {"@value": "Here is a longer description of the object", "@language": "en"}}
 
 module.exports = router;
