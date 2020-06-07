446c21c40d6083632f2a3b09bd2f50238c8deba6
Fabian <bart@macMini.local>
2020-06-07 18:21:57 +0200

    Recursive validation

--- src/validation/validation.ts
+++ src/validation/validation.ts
@@ -2,54 +2,97 @@ import * as Router from 'koa-router';
 import * as Ajv from 'ajv';
 import * as fs from 'fs';
 import * as http from 'http';
+import {ErrorObject} from "ajv";
 
 const router: Router = new Router();
 
 router.get('/validate', async ctx => {
-
     const manifestUrl = ctx.request.query.manifest;
-    if (!manifestUrl || !isUrl(manifestUrl)) {
-        ctx.status = 400;
-        ctx.body = 'Wrong input';
+    ctx.body = await validateUrl(manifestUrl);
+});
+
+interface IResult {
+    [key: string]: Array<ErrorObject> | string;
+}
+
+async function validateUrl(manifestUrl?: string, result?: IResult) {
+
+    if (!result) {
+        result = {};
     }
 
-    let manifestData = await new Promise((resolve, reject) => {
-        http.get(manifestUrl, (response) => {
-            let chunksOfData: any = [];
+    if (!manifestUrl) {
+        result['-'] = 'Missing url';
+        return result;
+    }
 
-            response.on('data', (fragments) => {
-                chunksOfData.push(fragments);
-            });
+    if (manifestUrl === '') {
+        result['-'] = 'Empty url';
+        return result;
+    }
 
-            response.on('end', () => {
-                let response_body = Buffer.concat(chunksOfData);
+    if (!manifestUrl || !isUrl(manifestUrl)) {
+        result['-'] = 'Invalid url';
+        return result;
+    }
 
-                // promise resolved on success
-                resolve(response_body.toString());
-            });
+    try {
+        let manifestData = await new Promise((resolve, reject) => {
+            http.get(manifestUrl, (response) => {
+                let chunksOfData: any = [];
 
-            response.on('error', (error) => {
-                // promise rejected on error
-                reject(error);
+                response.on('data', (fragments) => {
+                    chunksOfData.push(fragments);
+                });
+
+                response.on('end', () => {
+                    let response_body = Buffer.concat(chunksOfData);
+
+                    // promise resolved on success
+                    resolve(response_body.toString());
+                });
+
+                response.on('error', (error) => {
+                    // promise rejected on error
+                    reject(error);
+                });
             });
         });
-    });
 
 
-    const rawdata = fs.readFileSync(__dirname + '/iiif_3_0.json');
-    const schema = JSON.parse(rawdata.toString());
+        const rawdata = fs.readFileSync(__dirname + '/iiif_3_0.json');
+        const schema = JSON.parse(rawdata.toString());
+        const data: any = JSON.parse(manifestData.toString());
 
-    const ajv = new Ajv({logger: false});
+        const ajv = new Ajv({logger: false});
 
-    ajv.validate(schema, JSON.parse(manifestData.toString())); // true
-    const errors = ajv.errors; // true
+        ajv.validate(schema, data);
+        let errors = ajv.errors;
 
-    if (errors) {
-        ctx.body = errors;
-    } else {
-        ctx.body = 'Validation successful';
+        if (errors) {
+            result[manifestUrl] = errors;
+        } else {
+            result[manifestUrl] = 'Validation successful';
+        }
+
+        if (data.hasOwnProperty('items')) {
+            for (const item of data.items) {
+                if (result.hasOwnProperty(item.id)) {
+                    result[item.id] = 'Loop';
+                    continue;
+                }
+                const childResult = await validateUrl(item.id, result);
+                result = Object.assign(result, childResult);
+            }
+        }
+
+        return result;
+    } catch (e) {
+        result[manifestUrl] = e.message;
+        return result;
     }
-});
+}
+
 
 function isUrl(input: string) {
     let url;
