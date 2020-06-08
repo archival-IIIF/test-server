c7cd96643b13d7976733d529869e1003b658947b
Fabian <bart@macMini.local>
2020-06-08 06:58:07 +0200

    presenter builder added

--- src/emptyFolder/emptyFolderV3.ts
+++ src/emptyFolder/emptyFolderV3.ts
@@ -1,32 +1,23 @@
 import * as Router from 'koa-router';
+import Collection from "../presentation-builder/v3/Collection";
 
 const prefix = '/iiif/v3';
 const router: Router = new Router({prefix});
 
 router.get('/collection/emptyFolder', ctx => {
-    ctx.body = {
-        id: ctx.request.origin + ctx.request.url,
-        type: 'Collection',
-        label: {en: ['Empty folder test case']},
-        '@context': 'http://iiif.io/api/presentation/3/context.json',
-        items: [
-            {
-                id: ctx.request.origin + prefix + '/collection/emptyFolder2',
-                type: 'Collection',
-                label: {en: ['Empty folder']},
-            }
-        ]
-    };
+    const c = new Collection(ctx.request.origin + ctx.request.url, 'Empty folder test case');
+    c.setContext('http://iiif.io/api/presentation/3/context.json');
+    c.setItems([
+        new Collection(ctx.request.origin + prefix + '/collection/emptyFolder2', 'Empty folder')
+    ])
+    ctx.body = c;
 });
 
 router.get('/collection/emptyFolder2', ctx => {
-    ctx.body = {
-        id: ctx.request.origin + ctx.request.url,
-        type: 'Collection',
-        label: {en: ['Empty folder']},
-        '@context': 'http://iiif.io/api/presentation/3/context.json',
-        partOf: [{id: ctx.request.origin + prefix + '/collection/emptyFolder', type: 'Collection'}]
-    };
+    const c = new Collection(ctx.request.origin + ctx.request.url, 'Empty folder');
+    c.setContext('http://iiif.io/api/presentation/3/context.json');
+    c.setParent(ctx.request.origin + prefix + '/collection/emptyFolder', 'Collection');
+    ctx.body = c;
 });
 
 export default router.routes();
