72c3c085cfe754c114d09579672cee82e980a137
Fabian <bart@macMini.local>
2020-06-09 08:41:21 +0200

    More presenter builder

--- src/audioVideo/audioVideoV2.ts
+++ src/audioVideo/audioVideoV2.ts
@@ -1,125 +1,20 @@
 import * as Router from 'koa-router';
-import * as path from 'path';
-import download from '../lib/Download';
+import {getAudioVideo, getDieInternationale, getF113} from "./audioVideo";
+import {transformCollectionToV2, transformFileManifestToV2} from "../lib/Transform";
 
 const prefix = '/iiif/v2';
 const router: Router = new Router({prefix});
 
 router.get('/collection/audioVideo', ctx => {
-    ctx.body = {
-        '@id': ctx.request.origin + prefix + '/collection/audioVideo',
-        '@type': 'sc:Collection',
-        label: 'Audio & video test case',
-        '@context': 'http://iiif.io/api/collection/2/context.json',
-        license: 'http://creativecommons.org/licenses/by-sa/3.0/',
-        manifests: [
-            {
-                '@id': ctx.request.origin + prefix + '/manifest/die_internationale_as_mp3',
-                '@type': 'sc:Manifest',
-                label: 'Die Internationale as mp3.mp3',
-                thumbnail: {
-                    '@id': ctx.request.origin + '/file-icon/mp3.svg',
-                    format: 'image/svg+xml'
-                }
-            },
-            {
-                '@id': ctx.request.origin + prefix + '/manifest/f113',
-                '@type': 'sc:Manifest',
-                label: 'F113.mp4',
-                thumbnail: {
-                    '@id': ctx.request.origin + '/file-icon/mp4.svg',
-                    format: 'image/svg+xml'
-                }
-            }
-        ]
-    };
+    ctx.body = transformCollectionToV2(getAudioVideo(ctx, prefix));
 });
 
-
 router.get('/manifest/die_internationale_as_mp3', ctx => {
-    ctx.body = {
-        '@id': ctx.request.origin + ctx.request.url,
-        '@type': 'sc:Manifest',
-        'label': 'Die_Internationale as mp3.mp3',
-        '@context': 'http://iiif.io/api/collection/2/context.json',
-        'license': 'http://creativecommons.org/licenses/by-sa/3.0/',
-        'within': ctx.request.origin + prefix + '/collection/audioVideo',
-        'metadata': [
-            {
-                'label': 'Original file type',
-                'value': '<a href=\"https://www.nationalarchives.gov.uk/PRONOM/Format/proFormatSearch.aspx?status=detailReport&id=687\">MPEG 1/2 Audio Layer 3 (.mp3)</a>'
-            },
-            {
-                'label': 'Original file size',
-                'value': '242.04 KB'
-            },
-            {
-                'label': 'Original modification date',
-                'value': 'July 11th 2018'
-            }
-        ],
-        thumbnail: {
-            '@id': ctx.request.origin + '/file-icon/mp3.svg',
-            format: 'image/svg+xml'
-        },
-        'mediaSequences': [{
-            '@id': ctx.request.origin + prefix + '/sequence/die_internationale_as_mp3',
-            '@type': 'ixif:MediaSequence',
-            'elements': [{
-                '@id': ctx.request.origin + '/file/die_internationale_as_mp3',
-                '@type': 'dctypes:Sound',
-                'format': 'audio/mpeg',
-                'rendering': {
-                    '@id': ctx.request.origin + '/file/die_internationale_as_mp3/original',
-                    'label': 'Original copy',
-                    'format': 'audio/mpeg'
-                }
-            }]
-        }]
-    };
+    ctx.body = transformFileManifestToV2(getDieInternationale(ctx, prefix));
 });
 
 router.get('/manifest/f113', ctx => {
-    ctx.body = {
-        '@id': ctx.request.origin + ctx.request.url,
-        '@type': 'sc:Manifest',
-        'label': 'F113.mp4',
-        '@context': 'http://iiif.io/api/collection/2/context.json',
-        'license': 'http://creativecommons.org/licenses/by-sa/3.0/',
-        'within': ctx.request.origin + prefix + '/collection/audioVideo',
-        'metadata': [
-            {
-                'label': 'Original file type',
-                'value': '<a href=\"https://www.nationalarchives.gov.uk/PRONOM/Format/proFormatSearch.aspx?status=detailReport&id=924\">MPEG-4 Media File (.f4a, .f4v, .m4a, .m4v, .mp4)</a>'
-            },
-            {
-                'label': 'Original file size',
-                'value': '6.86 MB'
-            },
-            {
-                'label': 'Original modification date',
-                'value': 'July 11th 2018'
-            }
-        ],
-        thumbnail: {
-            '@id': ctx.request.origin + '/file-icon/mp3.svg',
-            format: 'image/svg+xml'
-        },
-        mediaSequences: [{
-            '@id': ctx.request.origin + prefix + '/sequence/f113',
-            '@type': 'ixif:MediaSequence',
-            'elements': [{
-                '@id': ctx.request.origin + '/file/f113',
-                '@type': 'dctypes:Document',
-                'format': 'video/mp4',
-                'rendering': {
-                    '@id': ctx.request.origin + '/file/f113/original',
-                    'label': 'Original copy',
-                    'format': 'video/mp4'
-                }
-            }]
-        }]
-    };
+    ctx.body = transformFileManifestToV2(getF113(ctx, prefix));
 });
 
 export default router.routes();
