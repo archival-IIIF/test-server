e6c4f37bd9be2cb5e2abebf05273ba7411d933e8
Fabian <bart@macMini.local>
2020-06-17 12:28:23 +0200

    Minor

--- src/nestedStructure/nestedStructure.ts
+++ src/nestedStructure/nestedStructure.ts
@@ -6,31 +6,28 @@ import {addCollectionRoute} from "../lib/Route";
 const router: Router = new Router();
 
 const c1111 = new RootCollection('/collection/nestedStructure1111', 'Folder Level 1.1.1.1');
-c1111.setParent('/collection/nestedStructure111', 'Collection');
-addCollectionRoute(router, c1111);
 
 const c111 = new RootCollection('/collection/nestedStructure111', 'Folder Level 1.1.1');
-c111.setParent('/collection/nestedStructure11', 'Collection');
 c111.setItems(c1111)
-addCollectionRoute(router, c111);
+c1111.setParent(c111.id, 'Collection');
 
 const c12 = new RootCollection('/collection/nestedStructure12', 'Folder Level 1.2');
-c12.setParent('/collection/nestedStructure', 'Collection');
-addCollectionRoute(router, c12);
 
 const c11 = new RootCollection( '/collection/nestedStructure11', 'Folder Level 1.1');
-c11.setParent('/collection/nestedStructure', 'Collection');
 c11.setItems(c111);
-addCollectionRoute(router, c11);
-
+c111.setParent(c11.id, 'Collection');
 
 const c1 = new RootCollection('/collection/nestedStructure', 'Folder Level 1');
-c1.setItems([
-    c11,
-    c12
-]);
-addCollectionRoute(router, c1);
+c1.setItems([c11, c12]);
+c11.setParent(c1.id, 'Collection');
+c12.setParent(c1.id, 'Collection');
 
+
+addCollectionRoute(router, c1);
+addCollectionRoute(router, c11);
+addCollectionRoute(router, c111);
+addCollectionRoute(router, c1111);
+addCollectionRoute(router, c12);
 
 
 export default router.routes();
